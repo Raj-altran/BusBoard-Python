3c9b7e97cdb24ad9c0a05cc0fce9f5492bf5553a
rthethi <rajpal.thethi@altran.com>
2021-09-20 17:17:49 +0100

    User interface

--- main.py
+++ main.py
@@ -6,15 +6,30 @@ from constants import *
 def main():
     print("Welcome to BusBoard.")
 
-    bus_id = "0180BAC30345"
+    live = True
+    while live:
+        user_input = input("Enter a atco code: ")
+        if user_input == "quit":
+            live = False
+            break
+        elif user_input == "":
+            bus_id = "0180BAC30345"
+        else:
+            bus_id = user_input
 
-    r = requests.get(f'https://transportapi.com/v3/uk/bus/stop/{bus_id}/live.json?app_id={APP_ID}&app_key={APP_KEY}&group=route&nextbuses=yes')
-    request = r.json()
+        try:
+            request = request_bus_stop(bus_id)
+            busstop = BusStop(request)
+            busstop.sort_departures()
+            busstop.printout_departures()
+        except KeyError:
+            print("Bad input, try again.")
 
-    busstop = BusStop(request)
-    busstop.sort_departures()
 
-    busstop.printout_departures()
+def request_bus_stop(id):
+    r = requests.get(
+        f'https://transportapi.com/v3/uk/bus/stop/{id}/live.json?app_id={APP_ID}&app_key={APP_KEY}&group=route&nextbuses=yes')
+    return r.json()
 
 
 if __name__ == "__main__": main()
