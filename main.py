e5e96b8b9c36aa9c3010ddd7c1d71926f6b57305
rthethi <rajpal.thethi@altran.com>
2021-09-21 11:37:27 +0100

    put all resquest functions in one file

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import requests
 from busStop import BusStop
-from constants import *
+from requesters import *
 import web
 from flask import Flask, render_template, request
 
 
 def main():
-    app = Flask(__name__)
-    app.run()
+    # app = Flask(__name__)
+    # app.run()
 
     live = True
     while live:
@@ -16,13 +15,7 @@ def main():
             live = False
             break
         elif user_input == "test":
-            bus_ids = ["0180BAC30345",
-                       "0180BAC30342",
-                       "0180BAC30601",
-                       "0180BAC31197",
-                       "0180BAC30587",
-                       "0180BAZ02395",
-                       "0180BAC30614"]
+            bus_ids = TEST_ATCOCODES
         elif user_input == "":
             bus_ids = ["0180BAC30345"]
         else:
@@ -31,6 +24,7 @@ def main():
             bus_ids = atcocodes
 
         for bus_id in range(len(bus_ids)):
+            request = "Error"
             try:
                 request = request_bus_stop(bus_ids[bus_id])
                 bus_stop = BusStop(request)
@@ -40,48 +34,12 @@ def main():
                 print_error(request)
 
 
-def request_bus_stop(id):
-    r = requests.get(
-        f'https://transportapi.com/v3/uk/bus/stop/{id}/live.json?app_id={APP_ID}&app_key={APP_KEY}&group=route&nextbuses=yes')
-    return r.json()
-
-
 def print_error(json):
     if "error" in json:
         print(json["error"])
     else:
         print("Bad input, try again.")
     print("-" * 20)
-
-
-def postcode_to_LatLong(post_code):
-    post_code = post_code.replace(" ", "")
-    r = requests.get(f"http://api.postcodes.io/postcodes?q={post_code}")
-    data = r.json()
-    if data["result"] is None:
-        return None, None
-    else:
-        lat = data["result"][0]["latitude"]
-        long = data["result"][0]["longitude"]
-        return lat, long
-
-
-def latLong_to_Atcocodes(lat, long):
-    r = requests.get(
-        f"http://transportapi.com/v3/uk/places.json?app_id={APP_ID}&app_key={APP_KEY}&lat={lat}&lon={long}&type=bus_stop")
-    data = r.json()
-
-    output = []
-    if "error" in data:
-        print("input error")
-        print(data["error"])
-        print("-" * 20)
-    else:
-        bus_stops = data["member"]
-        for stop in bus_stops:
-            atcocode = stop["atcocode"]
-            output.append(atcocode)
-    return output[:2]
 
 
 if __name__ == "__main__": main()
---
+++ requesters.py
@@ -0,0 +1,38 @@
+import requests
+from constants import *
+
+
+def request_bus_stop(id):
+    r = requests.get(
+        f'https://transportapi.com/v3/uk/bus/stop/{id}/live.json?app_id={APP_ID}&app_key={APP_KEY}&group=route&nextbuses=yes')
+    return r.json()
+
+
+def postcode_to_LatLong(post_code):
+    post_code = post_code.replace(" ", "")
+    r = requests.get(f"http://api.postcodes.io/postcodes?q={post_code}")
+    data = r.json()
+    if data["result"] is None:
+        return None, None
+    else:
+        lat = data["result"][0]["latitude"]
+        long = data["result"][0]["longitude"]
+        return lat, long
+
+
+def latLong_to_Atcocodes(lat, long, limit=2):
+    r = requests.get(
+        f"http://transportapi.com/v3/uk/places.json?app_id={APP_ID}&app_key={APP_KEY}&lat={lat}&lon={long}&type=bus_stop")
+    data = r.json()
+
+    output = []
+    if "error" in data:
+        print("input error")
+        print(data["error"])
+        print("-" * 20)
+    else:
+        bus_stops = data["member"]
+        for stop in bus_stops:
+            atcocode = stop["atcocode"]
+            output.append(atcocode)
+    return output[:limit]
